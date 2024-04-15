5a43332dcde9fb8e8291f41568f5b6ab35462aa1
fz3002 <franc3002@gmail.com>
2024-04-15 13:24:48 +0200

    Clean up

--- main.py
+++ main.py
@@ -6,64 +6,22 @@ import os
 
 
 class Perceptron:
-    """ A class to represent a Perceptron
-    
-        ...
-       
-        Attributes
-        ----------
-        alpha : float
-            learning constant
-        weights : list
-            vector (as list) of weights
-        activating_result : str
-            name of the class that should activate perceptron
-        
-        Methods
-        -------
-        __set_weights(number_of_attributes):
-            Sets random weights
-        compute(vector) : 
-            Computes net value and returns verdict for given vector
-        learn(good_result, vector) : 
-            Modifies weights and threshold using delta funcition
-
-    """
 
     def __init__(self, alpha, activating_result, number_of_attributes) :
-        """Contstructs all necessary attributes
-        
-            Args: 
-                alpha(float): learning constant
-                activating_result(str) : name of the class that should activate perceptron
-                number_of_attributes(int): number of entries of perceptron
-        """
+  
         
         self.alpha = alpha
         self.weights = []
         self.activating_result = activating_result
         self.__set_weights(number_of_attributes)
         
-    def __set_weights(self, number_of_attributes):
-        """Sets begining weights as random generated numbers
-
-        Args:
-            number_of_attributes (int): number of weights to generate (without threshold)
-        """        
+    def __set_weights(self, number_of_attributes):     
         
         for i in range(number_of_attributes):
             self.weights.append(random.randint(0, 1))
         self.weights.append(0)
 
-    def compute(self, vector):
-        """Computes output of perceptron
-
-        Args:
-            vector (list): vector to compute output from
-
-        Returns:
-            int: 1 activated or 0 if not
-        """        
+    def compute(self, vector):      
         
         net = self.get_net(vector)
         if net >= 0:
@@ -81,27 +39,17 @@ class Perceptron:
         return net
 
     def learn(self, good_result, vector):
-        """Function that teaches percpetron by changing weights and threshold using delta function
 
-        Args:
-            good_result (int): 0 or 1 depanding on the result given in train_set in Trainer
-            vector (list): vector to compute and learn on
-        """        
         prev_result = self.compute(vector)
         
         _vector = vector + [-1]
         
-        #print(_vector)
-        
         should_activate = 0
         if good_result == self.activating_result: should_activate = 1
         
-        #print(self, good_result, " ", self.activating_result, should_activate, prev_result)
         for i in range(len(self.weights)):
             
             self.weights[i] += ((should_activate - prev_result) * self.alpha * float(_vector[i]))
-            
-        #print(self.weights)
             
 
 class NeuralNetwork:
@@ -151,38 +99,9 @@ class NeuralNetwork:
             
 
 class Trainer:
-    """A class to train the perceptron
-    
-        ...
-       
-        Attributes
-        ----------
-        percpeptron : Perceptron
-            perceptron to train
-        train_set_fname : str
-            name of train_set file
-        train_set : list
-            list of vector to train on
-        names_of_classes : list
-            list of names of classes in dataset
-        
-        Methods
-        -------
-        __set_names_of_classes() :
-            Finds and sets names of classes from dataset in correct order
-        train(number_of_trainings) :
-            Method to train perceptron on given train_set
-        
-    """    
     
     def __init__(self, neural_network, train_set): 
-        """Contructor of Trainer class
-        
 
-        Args:
-            perceptron (Perceptron): perceptron to train passed from ui
-            train_set_fname (str): name of file containing train_set
-        """        
         self.neural_network = neural_network
         self.names_of_classes = neural_network.classes   
         self.train_set = train_set
@@ -191,11 +110,7 @@ class Trainer:
         
 
     def train(self):
-        """Method that trains perceptron
 
-        Args:
-            number_of_trainings (int): number of passes perceptron has to do through train_set
-        """        
         for line in self.train_set:
             for percpetron in self.neural_network.perceptrons:
                 percpetron.learn(line[-1], line[:-1])
@@ -231,12 +146,7 @@ class DataSetCreator:
         return vector
     
     @staticmethod
-    def get_names_of_classes(data):
-        """Sets list of names of classes (index 0 nonactivating, 1 - activating)
-
-        Returns:
-            list: list of names of classes
-        """        
+    def get_names_of_classes(data):     
         
         classes = []
         
@@ -266,7 +176,6 @@ class Controller:
         neural_network = NeuralNetwork(0.05, classes, len(data_set[0])-1)
         trainer = Trainer(neural_network, data_set)
         for i in range(100):
-            print("training number: ", i)
             trainer.train()
         neural_network.normalize_weights()
         while(True):
